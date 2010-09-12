6f87b9929c3136a40bc9babbdeff21f0b8b6829b
Jaime Carrera <dongraft@gmail.com>
2010-09-12 15:59:57 -0400

    area y perimetro ok

--- vertex.cpp
+++ vertex.cpp
@@ -7,7 +7,7 @@ Vertex::Vertex(){
 }
 
 Vertex::Vertex(Numeric x, Numeric y){
-    qDebug() << "Agregando los numerics" << x.toString();
+    qDebug() << "Agregando los numerics" << x.toString() << "y " << y.toString();
     assert(x.getDen() != 0);
     assert(y.getDen() != 0);
     this->x = x;
